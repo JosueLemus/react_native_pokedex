665b558615d3ed6756a9f8efe388ae5d43345f7d
Josue Lemus Miranda <josuelemusmiranda@MacBook-Pro-de-Josue.local>
2024-01-11 18:38:09 -0400

    Create hook to get pokemon list

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,14 +1,33 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {pokemonApi} from '../api/pokemonApi';
+import {
+  PokemonPaginatedResponse,
+  Result,
+  SimplePokemon,
+} from '../interfaces/pokemonInterface';
 
 export const usePokemonPaginated = () => {
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    [],
+  );
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(nextPageUrl.current);
-    console.log(resp.data);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current,
+    );
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList(resp.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.forEach(poke => console.log(poke.url));
   };
   useEffect(() => {
     loadPokemons();
   }, []);
+
+  return {
+    setSimplePokemonList
+  }
 };
