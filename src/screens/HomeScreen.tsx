8ec346c3207a67fbf5eceaedf5dc76ece22f5ee4
Josue Lemus Miranda <josuelemusmiranda@MacBook-Pro-de-Josue.local>
2024-01-11 18:23:39 -0400

    Create request to get pokemon list

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {Image, Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+  usePokemonPaginated();
   return (
-    <View>
-        <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokeballBG}
+      />
+      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
+        Pokedex
+      </Text>
+    </>
+  );
+};
